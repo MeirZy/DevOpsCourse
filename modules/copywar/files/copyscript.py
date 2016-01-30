fa910410bb20e77a2ddf0c6b6f74451bec575c4b
MeirZ <meir.zyman@gmail.com>
2016-01-30 17:24:57 +0200

    Update copyscript.py

--- modules/copywar/files/copyscript.py
+++ modules/copywar/files/copyscript.py
@@ -5,7 +5,7 @@ import os
 
 def copywar(source_file, destination_path):
     # This copy source war file to destination tomcat folder
-    os.system("scp " + source_file + "meirz@websrv.eastus.cloudapp.azure.com:" + destination_path)
+    os.system("scp " + source_file + " meirz@websrv.eastus.cloudapp.azure.com:" + destination_path)
     
     return;
 
