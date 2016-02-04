1ea6516d55db4c89af9fca027bdd5a27e2707944
MeirZ <meir.zyman@gmail.com>
2016-02-04 19:11:43 +0200

    Update copyscript.py

--- modules/copywar/files/copyscript.py
+++ modules/copywar/files/copyscript.py
@@ -9,8 +9,8 @@ def main():
  source_file = sys.argv[1]
  dest_dir = sys.argv[2]    
  
- os.system("scp " + "meirz@buildsrv.dybetaznhvqetlla1bxv4v3w0c.bx.internal.cloudapp.net:"+ source_file  dest_dir)
- return;
+ os.system("scp " + "meirz@buildsrv.dybetaznhvqetlla1bxv4v3w0c.bx.internal.cloudapp.net:"+ source_file + " "+ dest_dir)
+ 
   
 if __name__ == '__main__':
 main()
